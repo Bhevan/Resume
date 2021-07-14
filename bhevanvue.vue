3ac9e2c381b1e4389d8da58e4a8dc40d605a08fd
Bhevan <85232600+Bhevan@users.noreply.github.com>
2021-07-14 14:59:51 +0530

    Add files via upload

---
+++ bhevanvue.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="document-wrapper ">
+    <div class="absolute right top text-left">
+      <a class="block" href="https://linkedin.com/in/bhevan-bhevan-6a2180182/"><i class="fa fa-linkedin"></i> /in/nareshpingale</a>
+      <a class="block" href="https://github.com/Bhevan/Resume/"><i class="fa fa-github"></i> /Kesani Bhevan</a>
+    </div>
+    <div >
+      <h1>Kesani Bhevan</h1>
+      <div>
+        <div >
+          
+          <span>+91 7013154545</span>
+        </div>
+        <div >
+          Young, talented, passionate programmer with an eagerness for learning new technologies and building products carving better future.
+        </div>
+      </div>
+    </div>
+    <div class="clearfix">
+      <div class="w50 float-left md-collapse">
+        <h4>Professional Skills</h4>
+        <div>
+          <ul>
+            <li class="clearfix">
+              <span class="medium">JavaScript:</span>
+              <div class="float-right">
+                <i class="fa fa-star"></i>&nbsp;
+                <i class="fa fa-star"></i>&nbsp;
+                <i class="fa fa-star"></i>&nbsp;
+                <i class="fa fa-star"></i>&nbsp;
+                <i class="fa fa-star"></i>&nbsp;
+              </div>
+            </li>
+            <li class="clearfix">
+              <span class="medium">HTML & CSS:</span>
+              <div class="float-right">
+                <i class="fa fa-star"></i>&nbsp;
+                <i class="fa fa-star"></i>&nbsp;
+                <i class="fa fa-star"></i>&nbsp;
+                <i class="fa fa-star"></i>&nbsp;
+                <i class="fa fa-star"></i>&nbsp;
+              </div>
+            </li>
+            <li class="clearfix">
+              <span class="medium">Angular & AngularJs:</span>
+              <div class="float-right">
+                <i class="fa fa-star"></i>&nbsp;
+                <i class="fa fa-star"></i>&nbsp;
+                <i class="fa fa-star"></i>&nbsp;
+                <i class="fa fa-star"></i>&nbsp;
+                <i class="fa fa-star-o"></i>&nbsp;
+              </div>
+            </li>
+            <li class="clearfix">
+              <span class="medium">React:</span>
+              <div class="float-right">
+                <i class="fa fa-star"></i>&nbsp;
+                <i class="fa fa-star"></i>&nbsp;
+                <i class="fa fa-star"></i>&nbsp;
+                <i class="fa fa-star-o"></i>&nbsp;
+                <i class="fa fa-star-o"></i>&nbsp;
+              </div>
+            </li>
+            <li class="clearfix">
+              <span class="medium">Build Tools (Gupl, Webpack):</span>
+              <div class="float-right">
+                <i class="fa fa-star"></i>&nbsp;
+                <i class="fa fa-star"></i>&nbsp;
+                <i class="fa fa-star"></i>&nbsp;
+                <i class="fa fa-star"></i>&nbsp;
+                <i class="fa fa-star-o"></i>&nbsp;
+              </div>
+            </li>
+            <li class="clearfix">
+              <span class="medium">Git:</span>
+              <div class="float-right">
+                <i class="fa fa-star"></i>&nbsp;
+                <i class="fa fa-star"></i>&nbsp;
+                <i class="fa fa-star"></i>&nbsp;
+                <i class="fa fa-star-o"></i>&nbsp;
+                <i class="fa fa-star-o"></i>&nbsp;
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="w50 float-right md-collapse">
+        <h4>Employment History</h4>
+        <ul>
+          <li>
+            <div class="bold">Vkraft<span class="italic medium"> | may-05-0-2021-30-09-2021</span></div>
+            <div class="medium">intern</div>
+            <div class="italic f14">Handling The Operations With Vuejs,Reactjs,Angularnode.</div>
+          </li>
+         
+        </ul>
+      </div>
+      <div class="w50 float-left md-collapse">
+        <h4>Education</h4>
+        <ul>
+          <li>
+            <span class="bold">Kl University, Guntur</span><span class="italic" > - May 2022</span>
+            <div class="medium">Bachelor's of Computer Application(Information Technology)</div>
+            <div class="italic f14">During the 3 years of education i discovered my passion for technology building applications for various different platforms.</div>
+            <ul>
+                </ul>
+              </li>
+              <li>
+                <div class="medium">Other Social Activies:</div>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ExportDocument',
+  data () {
+    return {
+      msg: 'Welcome to Your Print App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .document-wrapper{
+    max-width: 900px;
+    margin: 0px auto;
+  }
+</style>
